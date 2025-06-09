43156a7e8b376a2eaeb88bd9749a3e1f46cb6bc6
jasmine <its.jassy@pm.me>
2025-06-09 11:10:57 +0800

    update help message

--- src/inbuilt.rs
+++ src/inbuilt.rs
@@ -6,7 +6,20 @@ pub fn cd(args: &[String]) {
 }
 
 pub fn help() {
-    println!("This is an example help message")
+    println!("RuSH v0.1.0");
+    println!("---------------------");
+    println!("Available Commands:");
+    println!("  cd            - Change directory");
+    println!("  help          - Display this help message");
+    println!("  exit          - Exit the shell");
+    println!("  <command>     - Execute a command (e.g., ls, echo, etc.)");
+
+    println!();
+    println!("Customization Options:");
+    println!("  aliases       - Define custom aliases for commands");
+
+    println!();
+    println!("Note: This is a basic implementation. More features and options will be added in future versions.");
 }
 
 pub fn exit() {
